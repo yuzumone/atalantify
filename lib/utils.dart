53c04fb23f92a4fa2337a1cf30855571a32bdd57
yuzumone <yuzumone@outlook.com>
2019-09-17 22:21:34 +0900

    Add to store and get token

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,9 +1,13 @@
 import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:url_launcher/url_launcher.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'model/token.dart';
 
+final String tokenKey = 'token';
+final String expiresKey = 'expires';
+final String refreshKey = 'refresh';
 
 dynamic getKey() async {
   var keyJson = await rootBundle.loadString('assets/.apikeys');
@@ -18,3 +22,21 @@ void launchURL(String url) async {
   }
 }
 
+void setToken(Token token) async {
+  SharedPreferences _prefs = await SharedPreferences.getInstance();
+  await _prefs.setString(tokenKey, token.token);
+  await _prefs.setInt(expiresKey, token.expires);
+  await _prefs.setString(refreshKey, token.refresh);
+}
+
+Future<Token> getToken() async {
+  SharedPreferences _pref = await SharedPreferences.getInstance();
+  var token = _pref.getString(tokenKey);
+  var expires = _pref.getInt(expiresKey);
+  var refresh = _pref.getString(refreshKey);
+  if (token == null || expires == null || refresh == null) {
+    return null;
+  } else {
+    return Token(token, expires, refresh);
+  }
+}
